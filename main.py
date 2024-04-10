8f82832a82fa8b38c74ae93c82ed5fd3f1a8ac65
tundewilly <tundewilly2010@gmail.com>
2024-04-10 11:11:19 +0100

    Few changes

---
+++ main.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+calculation = ""
+
+
+def add_to_calculation(symbol):
+    pass
+
+
+def evaluate_calculation():
+    pass
+
+
+def clear_field():
+    pass
+
+root = tk.Tk()
+root / geometry("300x275")
+root.mainloop()
